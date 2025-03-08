38882f2f9659a0f97e9a75bfbad96a932568204f
Akhilesh Yadav <ydvakhilesh16@gmail.com>
2025-03-08 11:33:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return(
+    <div className="bg-slate-900 min-h-screen font-serif text-4xl font-bold text-slate-300 w-screen text-center py-10">
+      Hello
+    </div>
+  )
+}
+
+export default App
