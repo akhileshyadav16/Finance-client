8037f8edd40de475f111da0e528c8be77a335839
Akhilesh Yadav <ydvakhilesh16@gmail.com>
2025-03-08 13:18:33 +0530

    add signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import Navbar from "./component/Navbar"
 import { Routes,Route } from "react-router-dom"
+import Signup from "./pages/Signup"
+import Login from "./pages/Login"
 
 function App() {
   return(
     <div>
       <Navbar/>
-
+      <Routes>
+        <Route path="/signup" element={<Signup/>} />
+        <Route path="/login" element={<Login/>} />
+      </Routes>
     </div>
   )
 }
