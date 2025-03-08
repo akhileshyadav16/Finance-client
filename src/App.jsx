c9625fbe53829509d9b5faa42f44fd160b7fbf51
Akhilesh Yadav <ydvakhilesh16@gmail.com>
2025-03-08 13:24:19 +0530

    add dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import Navbar from "./component/Navbar"
 import { Routes,Route } from "react-router-dom"
 import Signup from "./pages/Signup"
 import Login from "./pages/Login"
+import Expenses from "./pages/Expenses"
 
 function App() {
   return(
     <div>
       <Navbar/>
       <Routes>
+        <Route path="/" element={<Expenses/>} />
         <Route path="/signup" element={<Signup/>} />
         <Route path="/login" element={<Login/>} />
       </Routes>
